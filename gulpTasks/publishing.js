ecacfb949a3124bb55b7b29a3ac43b38fcfa48b7
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-14 14:40:01 -0300

    Update publishing.js

--- gulpTasks/publishing.js
+++ gulpTasks/publishing.js
@@ -49,7 +49,7 @@ gulp.task('upload-binaries', cb => {
   console.info('Checking Github releases...');
   // query github releases
   got(
-    `https://api.github.com/repos/ethereum/mist/releases?access_token=${GITHUB_TOKEN}`,
+    `https://api.github.com/repos/puffscoin/mist/releases?access_token=${GITHUB_TOKEN}`,
     { json: true }
   )
     // filter draft with current version's tag
@@ -89,7 +89,7 @@ gulp.task('upload-binaries', cb => {
 
         return (
           githubUpload({
-            url: `https://uploads.github.com/repos/ethereum/mist/releases/${
+            url: `https://uploads.github.com/repos/puffscoin/mist/releases/${
               draft.id
             }/assets{?name}`,
             token: [GITHUB_TOKEN],
@@ -114,7 +114,7 @@ gulp.task('upload-binaries', cb => {
                   })
                   .join('\n');
                 got.patch(
-                  `https://api.github.com/repos/ethereum/mist/releases/${
+                  `https://api.github.com/repos/puffscoin/mist/releases/${
                     draft.id
                   }?access_token=${GITHUB_TOKEN}`,
                   {
